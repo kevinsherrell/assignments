63238c74b04f8756575a17d721603d7f9c84c7e3
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-01-24 17:10:01 -0700

    class 2 commit

---
+++ excercises/hello-class/app.js
@@ -0,0 +1,13 @@
+// var sayHello = function(){
+//     console.log("hello class")
+// }
+// console.log(sayHello())
+name = "Kevin"
+age = "32"
+greeting = "Hello"
+question = "how do you like being " + age
+function sayHello(name){
+    console.log(greeting + " " + name + " ," + " " + question + "?");
+}
+
+sayHello(name)
