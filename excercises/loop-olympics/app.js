994208b39ead5e5e32f8441e185b4b3ec4d5cead
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-01-28 14:05:03 -0700

    added new assignment

---
+++ excercises/loop-olympics/app.js
@@ -0,0 +1,60 @@
+// PRELIMINARIES
+// Write a for loop that prints to the cosole the numbers 0 through 9
+for (var i = 0; i <= 9; i++) {
+    console.log(i)
+}
+
+// Write a loop that prints to the console 9 through 0
+for (var i = 9; i >= 0; i--) {
+    console.log(i)
+}
+
+// Write a for loop that prints these fruits to the console ["banana", "orange", "apple", "kiwi"]
+var fruit = ["banana", "orange", "apple", "kiwi"];
+for (i = 0; i < fruit.length; i++) {
+    console.log(fruit[i])
+}
+
+// BRONZE MEDAL
+// Write a for loop that will push the numbers 0 though 9 to an array. 
+var arr = [];
+for(var i = 0 ;arr.length < 10 ; i++){
+    arr.push(i)
+    console.log(arr)
+}
+// Write a for loop that prints to the console only even numbers 0 though 100.
+for(var i = 0; i < 101; i += 2){
+    console.log(i)
+}
+// Write a for loop that will push every other fruit to an array. var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+for(i = 1; i < fruit.length; i += 2){
+    console.log(fruit[i])
+}
+// SILVER MEDAL
+// Write a loop that will print out all the names of the people of the people array.
+var peopleArray = [
+    {
+        name: "Harrison Ford",
+        occupation: "Actor",
+    },
+    {
+        name: "Justin Bieber",
+        occupation: "Singer",
+    },
+    {
+        name: "Vladimir Putin", 
+        occupation: "Politician",
+    },
+    {
+        name: "Opra",
+        occupation: "Entertainer",
+    }
+]
+console.log(peopleArray[1].name)
+
+
+// console.log(peopleArray.keys(peopleArray));
+// Write a loop that pushes the names into a names array, and the occupations into an occupations array.
+// Write a loop that pushes every other name to an array starting with "Harrison Ford", and every other occupation to another array starting with "Singer".
+
---
+++ excercises/test/app.js
@@ -0,0 +1,7 @@
+var wall = {
+    color: "white",
+    size: "large",
+    decoration: "sticky notes",
+    clean: false,
+  }
+  console.log(wall.color);
