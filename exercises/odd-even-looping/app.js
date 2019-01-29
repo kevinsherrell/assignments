cb822fb6a5e4f16345b6dee9b19452ad9d9a0b12
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-01-29 09:56:13 -0700

    odd even looping assignment

---
+++ exercises/odd-even-looping/app.js
@@ -0,0 +1,9 @@
+// Create a for loop that iterates though 101 numbers (from 0 - 100)./ If the current iteration is an Odd number, print "Odd" to the console, otherwise print "Even".
+for(i = 0; i < 101; i++){
+    if(i % 2 != 0){
+        console.log("Odd")
+    }
+    else(
+        console.log("Even")
+    )
+}
