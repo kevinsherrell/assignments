1ff06613050976720bf5d06c2094f2fb554aacf1
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-01-31 09:12:57 -0700

    folder

---
+++ exercises/strings-methods-exercise/app.js
@@ -0,0 +1,23 @@
+// Make a function that will return any given string into all caps followed by the same string all lowercase.
+function capitalizeAndLowerCase(a){
+    return a.toUpperCase() + a.toLowerCase()
+}
+console.log(capitalizeAndLowerCase("whats up"))
+
+// Make a function that returns a number half the length, and rounded down. You'll need to use Math.floor().
+function findMiddleIndex(a){
+    return Math.floor(a.length / 2)
+}
+console.log(findMiddleIndex("Hello"))
+
+// Make a function that uses slice() and the other functions you've written to return the first half of the string
+// function returnFirstHalf(a){
+//     return a.slice(0, a.length / 2)
+// }
+// console.log(returnFirstHalf("Hello"))
+
+// Make a function that takes a string and returns that string where the first half is capitalized, and the second half is lower cased. (If the string length is odd, capitalize the shorter of the first half.)
+
+function returnFirstHalf(a){
+    
+}
