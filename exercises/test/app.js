58b6a0c027b9f1929ba7eba9ccb2cae06494b5e1
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-02-04 12:24:01 -0700

    added greece flag

--- exercises/test/app.js
+++ exercises/test/app.js
@@ -1,7 +1,11 @@
-var wall = {
-    color: "white",
-    size: "large",
-    decoration: "sticky notes",
-    clean: false,
-  }
-  console.log(wall.color);
+function myFunction(){
+  var greeting = "hello"
+  return greeting
+}
+
+var myFunctionTwo= function(){
+  var greetingTwo = "Hello World"
+  return greetingTwo
+}
+console.log(myFunction())
+console.log(myFunctionTwo())
