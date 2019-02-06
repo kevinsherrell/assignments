7b1ce8a07d0fb9a4d5fec177744baed076111fb9
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-02-05 21:00:16 -0700

    finished mario pest control

--- projects/mario-pest-control/js/app.js
+++ projects/mario-pest-control/js/app.js
@@ -1,8 +1,31 @@
-var goombahs = document.getElementById("mario-form")
-var goombahCost = 5
-var bobOmbs = document.getElementById("bob-omb")
-var bobOmbCost = 7
-var cheepCheeps = document.getElementById("cheep-cheep")
-var cheepCheepCost = 11
+var goombahs = document.getElementById("goombah")
+var goombahTotal = goombahs.value * 5
+var bobOmbs = document['mario-form']['bob-omb']
+var bobOmbTotal = bobOmbs.value * 7
+var cheepCheeps = document['mario-form']['cheep-cheep']
+var cheepCheepTotal = cheepCheeps.value * 11
 var yoshiBtn = document.getElementById("yoshi-button")
+var totalPriceBox = document.getElementById("calculation")
+var totalPrice = 0
+
+totalPriceBox.textContent = 0
+
+
+
+
+goombahs.addEventListener('input', function () {
+    var value = Number(totalPriceBox.textContent)
+    totalPriceBox.textContent = value + goombahs.value * 5 
+})
+bobOmbs.addEventListener('input', function () {
+    var value = Number(totalPriceBox.textContent)
+    totalPriceBox.textContent = value + bobOmbs.value * 7
+})
+cheepCheeps.addEventListener('input', function () {
+    var value = Number(totalPriceBox.textContent)
+    totalPriceBox.textContent = value + cheepCheeps.value * 11
+})
+
+
+
 
