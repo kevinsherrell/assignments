ac2cd2cdd6f30a4d9cec6a6c9a473840ca5c7193
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-02-08 15:47:10 -0700

    revised calculator file

--- exercises/simple-html-calculator/js/app.js
+++ exercises/simple-html-calculator/js/app.js
@@ -8,9 +8,6 @@ var subtractionInputOne = document.getElementById('subtraction-input-1')
 var subtractionInputTwo = document.getElementById('subtraction-input-2')
 var multiplicationInputOne = document.getElementById('multiplication-input-1')
 var multiplicationInputTwo = document.getElementById('multiplication-input-2')
-var additionButton = document.getElementById('addition-submit')
-var subtractionButton = document.getElementById('subtraction-submit')
-var multiplicationButton = document.getElementById('multiplication-submit')
 var resultBox = document.getElementById('result-box')
 
 addition.addEventListener('submit', function(event){
