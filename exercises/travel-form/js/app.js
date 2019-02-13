4a71f09ca58c3f7a3212e39bcd726a332b142ee1
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-02-13 16:16:52 -0700

    finished travel-form using constructor functions

--- exercises/travel-form/js/app.js
+++ exercises/travel-form/js/app.js
@@ -1,22 +1,22 @@
-let travel = document.travelForm
-let firstName = document.getElementById('firstName').value
-
-
-
-
-
-
-
-
-
 // let travel = document.travelForm
-// let firstName = document.travelForm.firstName.value
-// let lastName = document.travelForm.lastName
-// let age = document.travelForm.age
-// let gender = document.travelForm.gender
+// let firstName = document.getElementById('firstName').value
 
-// let dietary = document.travelForm.dietary
-// let travelLocation = document.travelForm.location
+
+
+
+
+
+
+
+
+let travel = document.travelForm
+let firstName = document.travelForm.firstName
+let lastName = document.travelForm.lastName
+let age = document.travelForm.age
+let gender = document.travelForm.gender
+
+let dietary = document.travelForm.dietary
+let travelLocation = document.travelForm.location
 
 
 // travel.addEventListener('submit', function(event){
@@ -26,22 +26,31 @@ let firstName = document.getElementById('firstName').value
 
 
 // This Solution Does Not Work....ASK WHY!!!
-// class Person {
-//     constructor(firstName, lastName, age, gender, dietary, travelLocation) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.age = age;
-//         this.gender = gender;
-//         this.dietary = dietary;
-//         this.travelLocation = travelLocation;
-//     }
-//     personMessage() {
-//         return this
-//     }
-// }
-// let person1 = new Person(travel[0].value, travel[1].value, travel[2].value, travel[3].value, travel[4].value, travel[5].value)
 
-// travel.addEventListener('submit', function(event){
-// event.preventDefault()
-// alert(person1)
-// })
+class Person {
+    constructor(firstName, lastName, age, gender, dietary, travelLocation) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.dietary = dietary;
+        this.travelLocation = travelLocation;
+    }
+    personMessage() {
+        return this
+    }
+}
+
+travel.addEventListener('submit', function (event) {
+
+
+    event.preventDefault()
+    let person1 = new Person(firstName.value, lastName.value, age.value, gender.value, dietary, travelLocation.value)
+    let checked = []
+    for(i=0; i<person1.dietary.length; i++){
+        if(person1.dietary[i].checked){
+            checked.push(person1.dietary[i].value)
+        }
+    }
+    alert("First Name: " + firstName.value + '\n' + "Last Name:  " + lastName.value + "\n" + "Age: " + age.value + "\nGender: " + gender.value + "\nDietary Restrictions: " + checked + "\n" + "Travel Location: " + travelLocation.value)
+})
