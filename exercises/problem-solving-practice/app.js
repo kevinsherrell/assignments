22772fece8202c58b2266ef21364473f4141582b
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-02-16 23:11:49 -0700

    finished problem solving practice

---
+++ exercises/problem-solving-practice/app.js
@@ -0,0 +1,38 @@
+// Write a function that takes an array of numbers and returns the largest (without using Math.max())
+// let arr = [66, 7, 77, 8, 9, 33];
+
+// function largestNum(arr) {
+//     arr.sort(function (a, b) {
+//         return a - b
+//     })
+//     return arr[0]
+// }
+// console.log(largestNum(arr))
+
+// Write a function that takes an array of words and a character and return each word that has that letter present.
+
+
+// arr = ['hello', 'goodbye', 'froggy']
+// function hasLetter(arr, char){
+//     let result = []
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i].indexOf(char) !== -1){
+//         result.push(arr[i])
+//         }
+//     }
+//     return result
+
+// }
+// console.log(hasLetter(arr, 'b'))
+
+// Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+
+// function isDivisible(num1, num2){
+//     if(num1 % num2 === 0){
+//         return "These numbers is divisible! "
+//     }else{
+//         return "These numbers are not divisible!"
+//     }
+// }
+// console.log(isDivisible(44,2))
