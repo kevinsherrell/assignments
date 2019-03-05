edc40b2857ae5e7765ca7a130f575ed208d24593
Kevin Sherrell <k.sherrell.1028@gmail.com>
2019-03-05 09:43:30 -0700

    started name-entry

---
+++ exercises/name-entry/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1></h1>
+                <input type="text"/>
+                <button></button>
+                <ol></ol>
+            </div>
+        )
+    }
+}
+
+export default App
